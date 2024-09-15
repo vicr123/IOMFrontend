1a7304ebe2cf85abe5b03a041f7a6b9ebedd0167
Victor Tran <vicr12345@gmail.com>
2024-09-15 17:54:31 +1000

    Sync maps in background

--- src/main/java/com/vicr123/IOMFrontend/Commands/IOMCommand.java
+++ src/main/java/com/vicr123/IOMFrontend/Commands/IOMCommand.java
@@ -3,6 +3,7 @@ package com.vicr123.IOMFrontend.Commands;
 import com.loohp.imageframe.ImageFrame;
 import com.vicr123.IOMFrontend.Database.DatabaseManager;
 import com.vicr123.IOMFrontend.Database.Map;
+import com.vicr123.IOMFrontend.Database.Rotondo;
 import com.vicr123.IOMFrontend.IOMFrontendPlugin;
 import com.vicr123.IOMFrontend.Server.ServerRoot;
 import net.md_5.bungee.api.ChatColor;
@@ -16,6 +17,7 @@ import org.bukkit.entity.Player;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class IOMCommand implements CommandExecutor {
     ServerRoot server;
@@ -47,51 +49,51 @@ public class IOMCommand implements CommandExecutor {
         ArrayList<Map> toDelete = new ArrayList<>();
         ArrayList<Map> toCreate = new ArrayList<>();
 
-        //First, attempt to clean up the map database
-        //Delete any maps that are not found (deleted in-game)
         try {
-            for (Map map : db.getMapDao().queryForEq("associatedPlayer", player.getUniqueId().toString())) {
-                var imageMap = ImageFrame.imageMapManager.getFromCreator(player.getUniqueId()).stream().filter(im -> im.getMapIds().stream().anyMatch(id -> id == map.getId())).findAny().orElse(null);
-                if (imageMap == null) toDelete.add(map);
-            }
+            List<Map> allMaps = db.getMapDao().queryForAll();
+            List<Rotondo> allRotondos = db.getRotondoDao().queryForAll();
+            List<Map> playerMaps = db.getMapDao().queryForEq("associatedPlayer", player.getUniqueId().toString());
+
+            Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+                //First, attempt to clean up the map database
+                //Delete any maps that are not found (deleted in-game)
+                for (Map map : playerMaps) {
+                    var imageMap = ImageFrame.imageMapManager.getFromCreator(player.getUniqueId()).stream().filter(im -> im.getMapIds().stream().anyMatch(id -> id == map.getId())).findAny().orElse(null);
+                    if (imageMap == null) toDelete.add(map);
+                }
+
+                //Import any new maps that are not found (created in-game)
+                for (var imageMap : ImageFrame.imageMapManager.getFromCreator(player.getUniqueId())) {
+                    if (allMaps.stream().noneMatch(map -> ImageFrame.imageMapManager.getFromMapId((int) map.getId()) != null) &&
+                            allRotondos.stream().noneMatch(rotondo -> ImageFrame.imageMapManager.getFromMapId((int) rotondo.getId()) != null)) {
+                        //This map is not found
+                        Map map = new Map();
+                        map.setId(imageMap.getMapIds().get(0));
+                        map.setName(imageMap.getName());
+                        map.setAssociatedPlayer(player.getUniqueId().toString());
+                        map.setPictureResource("x");
+
+                        toCreate.add(map);
+                    }
+                }
+
+                try {
+                    db.getMapDao().delete(toDelete);
+                    db.getMapDao().create(toCreate);
+                } catch (SQLException e) {
+                    sender.sendMessage("An error occurred syncing your maps. The maps on the web UI may not be up to date.");
+                    e.printStackTrace();
+                }
+
+                TextComponent component = new TextComponent("Click to edit your maps");
+                component.setColor(ChatColor.GREEN);
+                component.setUnderlined(true);
+                component.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, server.rootUrl() + "?auth=" + server.tokenForPlayer((Player) sender)));
+                sender.spigot().sendMessage(component);
+            });
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-        //Import any new maps that are not found (created in-game)
-        for (var imageMap : ImageFrame.imageMapManager.getFromCreator(player.getUniqueId())) {
-            try {
-                if (db.getMapDao().queryForAll().stream().noneMatch(map -> ImageFrame.imageMapManager.getFromMapId((int) map.getId()) != null) &&
-                        db.getRotondoDao().queryForAll().stream().noneMatch(rotondo -> ImageFrame.imageMapManager.getFromMapId((int) rotondo.getId()) != null)) {
-                    //This map is not found
-                    Map map = new Map();
-                    map.setId(imageMap.getMapIds().get(0));
-                    map.setName(imageMap.getName());
-                    map.setAssociatedPlayer(player.getUniqueId().toString());
-                    map.setPictureResource("x");
-
-                    toCreate.add(map);
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
-            try {
-                db.getMapDao().delete(toDelete);
-                db.getMapDao().create(toCreate);
-            } catch (SQLException e) {
-                sender.sendMessage("An error occurred syncing your maps. The maps on the web UI may not be up to date.");
-                e.printStackTrace();
-            }
-
-            TextComponent component = new TextComponent("Click to edit your maps");
-            component.setColor(ChatColor.GREEN);
-            component.setUnderlined(true);
-            component.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, server.rootUrl() + "?auth=" + server.tokenForPlayer((Player) sender)));
-            sender.spigot().sendMessage(component);
-        });
 
         return true;
     }
