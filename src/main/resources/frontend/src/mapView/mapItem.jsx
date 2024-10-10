f543e7845bf5ad0e66cdc2a9f0e235f133faef4d
Victor Tran <vicr12345@gmail.com>
2024-10-10 18:14:13 +1100

    Convert mapItem to function component

--- src/main/resources/frontend/src/mapView/mapItem.jsx
+++ src/main/resources/frontend/src/mapView/mapItem.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import MapItemDropTarget from "./mapitemdroptarget";
+import React, {useEffect, useState} from "react";
 import Toast from "../Toast";
 import Styles from "./mapItem.module.css";
 
@@ -7,188 +6,103 @@ import GeneratedMap from "./generatedmap.svg";
 import {MapItemModal} from "./mapItemModal";
 import {Icon} from "../icon";
 
-class MapItem extends React.Component {
-    constructor(props) {
-        super(props);
+export default function MapItem({data, collection, isCollection, manager}) {
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
+    const [dragging, setDragging] = useState(false);
+    const [draggingThis, setDraggingThis] = useState(false);
+    const [modalOpen, setModalOpen] = useState(false);
 
-        this.state = {
-            dragging: false,
-            draggingThis: false,
-            width: 0,
-            height: 0,
-            modalOpen: false
-        };
-    }
-
-    componentDidMount() {
-        let img = new Image();
+    useEffect(() => {
+        const img = new Image();
         img.addEventListener("load", () => {
-            this.setState({
-                width: Math.ceil(img.naturalWidth / 128),
-                height: Math.ceil(img.naturalHeight / 128)
-            })
+            setWidth(Math.ceil(img.naturalWidth / 128));
+            setHeight(Math.ceil(img.naturalHeight / 128));
         });
-        img.src = this.props.data.pictureResource === "x" ? GeneratedMap : `/images/${this.props.data.pictureResource}`;
-    }
+        img.src = data.pictureResource === "x" ? GeneratedMap : `/images/${data.pictureResource}`;
+    }, []);
 
-    render() {
-        if (this.state.width === 0) {
-            return null;
-        } else {
-            return <div className={Styles.MapItemContainer}>
-                <div draggable={true}
-                     className={[Styles.MapItem, this.state.dragging ? Styles.Dragging : "", Object.keys(this.props.data.rotondos).length === 0 ? "" : Styles.MapItemRotondo].join(" ")}
-                     onDragEnter={this.dragEnter.bind(this)}
-                     onDrop={this.drop.bind(this)}
-                     onDragLeave={this.dragLeave.bind(this)}
-                     onDragOver={this.dragOver.bind(this)}
-                     onDragStart={this.dragStart.bind(this)}
-                     onDragEnd={this.dragEnd.bind(this)}
-                     onClick={this.click.bind(this)}>
-                    <img className={Styles.mapImage} src={this.props.data.pictureResource === "x" ? GeneratedMap : `/images/${this.props.data.pictureResource}`} />
-                </div>
-                <div className={Styles.MapItemInfo} onClick={this.expand.bind(this)}>
-                    <span>{this.props.data.name}</span>
-                    <div style={{flexGrow: "1"}}/>
-                    <Icon icon={"application-menu"}/>
-                </div>
-                <MapItemModal width={this.state.width} height={this.state.height} collection={this.props.collection} isCollection={this.props.isCollection}
-                              open={this.state.modalOpen} close={this.collapse.bind(this)} data={this.props.data}
-                              manager={this.props.manager}/>
-            </div>
-        }
-    }
-
-    async expand() {
-        this.setState({
-            modalOpen: true
-        })
-    }
-
-    async collapse() {
-        this.setState({
-            modalOpen: false
-        })
-    }
-
-    async click() {
-        if (Object.keys(this.props.data.rotondos).length !== 0) {
-            await this.giveRotondoMap(this.props.data.id)
-            return;
-        }
-        // try {
-        Toast.makeToast(<Toast title={"Map Obtained"} text={"The map is now in your inventory."} />)
-            await this.props.manager.giveMap(this.props.data.id);
-        // } catch {
-            // Toast.makeToast(<Toast title={"Could not obtain map"} text={"The map could not be obtained."} />)
-        // }
-    }
-
-    async deleteMap() {
-        if (window.confirm("Delete this map? Any mapsigns remaining on the world will cease to function correctly, and the map will be deleted from all public collections as well.")) {
-            await this.props.manager.deleteMap(this.props.data.id);
-            Toast.makeToast(<Toast title={"Map Deleted"} text={"The map was deleted."} />)
-        }
-    }
-
-    async removeCollection() {
-        if (window.confirm("Remove this map from the collection? Any placed maps will stay active.")) {
-            await this.props.manager.deleteCollection(this.props.data.id, this.props.collection);
-            Toast.makeToast(<Toast title={"Map removed from collection"} text={"The map was removed from the collection."} />)
-        }
-    }
-
-    async giveMap() {
-        await this.props.manager.giveMap(this.props.data.id);
-    }
-
-    async giveRotondoMap(id) {
-        Toast.makeToast(<Toast title={"Map Obtained"} text={"The map is now in your inventory."} />)
-        await this.props.manager.giveMap(id);
-    }
-
-    async setCatg() {
-        let catg = prompt("Name of category for this map:", this.props.data.category);
-        console.log(catg);
-        if (catg !== null) await this.props.manager.setCatg(this.props.data.id, catg);
-    }
-
-    async addCollection() {
-        let collection = prompt("Name of collection to add to:", this.props.data.category);
-        if (collection !== null) await this.props.manager.addToCollection(this.props.data.id, collection);
-    }
-
-    async setName() {
-        let name = prompt("Name of this map:", this.props.data.name);
-        if (name !== null) await this.props.manager.setName(this.props.data.id, name);
-    }
-
-    async createRotondo() {
-        await this.props.manager.createRotondo(this.props.data.id);
-    }
-
-    dragStart(e) {
-        e.dataTransfer.setData("application/x.vicr123.iomfrontend.mapid", this.props.data.id);
-        e.dataTransfer.setData("application/x.vicr123.iomfrontend.maporigin", this.props.isCollection ? "collection" : "category");
+    const dragStart = (e) => {
+        e.dataTransfer.setData("application/x.vicr123.iomfrontend.mapid", data.id);
+        e.dataTransfer.setData("application/x.vicr123.iomfrontend.maporigin", isCollection ? "collection" : "category");
         e.dataTransfer.dropEffect = "move";
 
-        this.setState({
-            draggingThis: true
-        });
+        setDraggingThis(true);
     }
 
-    dragEnd(e) {
-        this.setState({
-            draggingThis: false
-        });
+    const dragEnd = (e) => {
+        setDraggingThis(false);
     }
 
-    dragEnter(e) {
+    const dragEnter = (e) => {
         e.preventDefault();
         e.stopPropagation();
         let transfer = e.dataTransfer;
         transfer.dropEffect = "copy";
     }
 
-    dragLeave(e) {
+    const dragLeave = (e) => {
         e.preventDefault();
         e.stopPropagation();
 
-        this.setState({
-            dragging: false
-        });
+        setDragging(false);
     }
 
-    dragOver(e) {
+    const dragOver = (e) => {
         if (e.dataTransfer.types.includes("application/x.vicr123.iomfrontend.mapid")) return;
         e.preventDefault();
         e.stopPropagation();
 
-        this.setState({
-            dragging: true
-        });
+        setDragging(true);
     }
 
-    drop(e) {
+    const drop = (e) => {
         if (e.dataTransfer.types.includes("application/x.vicr123.iomfrontend.mapid")) return;
         try {
             e.preventDefault();
             e.stopPropagation();
 
-            this.setState({
-                dragging: false
-            });
+            setDragging(false);
 
             let transfer = e.dataTransfer;
             let file = [...transfer.items].filter(item => item.kind === "file")
                 .map(item => item.getAsFile())
                 .filter(file => ["image/png", "image/svg+xml", "image/gif"].includes(file.type))[0];
 
-            if (file) this.props.manager.updateImage(this.props.data.id, file);
+            if (file) manager.updateImage(data.id, file);
         } catch (exception) {
             alert("Couldn't update the map.");
         }
     }
-}
 
-export default MapItem;
+    const click = async () => {
+        Toast.makeToast(<Toast title={"Map Obtained"} text={"The map is now in your inventory."} />)
+        await manager.giveMap(data.id);
+    }
+
+    if (width === 0) {
+        return null;
+    } else {
+        return <div className={Styles.MapItemContainer}>
+            <div draggable={true}
+                 className={[Styles.MapItem, dragging ? Styles.Dragging : "", Object.keys(data.rotondos).length === 0 ? "" : Styles.MapItemRotondo].join(" ")}
+                 onDragEnter={dragEnter}
+                 onDrop={drop}
+                 onDragLeave={dragLeave}
+                 onDragOver={dragOver}
+                 onDragStart={dragStart}
+                 onDragEnd={dragEnd}
+                 onClick={click}>
+                <img className={Styles.mapImage} src={data.pictureResource === "x" ? GeneratedMap : `/images/${data.pictureResource}`} />
+            </div>
+            <div className={Styles.MapItemInfo} onClick={() => setModalOpen(true)}>
+                <span>{data.name}</span>
+                <div style={{flexGrow: "1"}}/>
+                <Icon icon={"application-menu"}/>
+            </div>
+            <MapItemModal width={width} height={height} collection={collection} isCollection={isCollection}
+                          open={modalOpen} close={() => setModalOpen(false)} data={data}
+                          manager={manager}/>
+        </div>
+    }
+}
